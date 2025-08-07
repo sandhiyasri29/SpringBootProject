ec0a6d6637ae562aa4fb43513a521f67305d5b6f
sandhiya <sandhiyasri2911@gmail.com>
2025-08-07 13:12:42 +0530

    Create SandhiyaDbbApplication.java

---
+++ SandhiyaDbbApplication.java
@@ -0,0 +1,13 @@
+package com.example.SandhiyaDBB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandhiyaDbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandhiyaDbbApplication.class, args);
+	}
+
+}
