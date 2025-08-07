e72fd22486e60cdfab369f79cb92a42b5d7363de
sandhiya <sandhiyasri2911@gmail.com>
2025-08-07 13:11:38 +0530

    Create RepoDb.java

---
+++ RepoDb.java
@@ -0,0 +1,9 @@
+package com.example.SandhiyaDBB.RepoDb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.SandhiyaDBB.Entity.Entitydb;
+
+public interface Repodb extends JpaRepository<Entitydb,Integer>{
+
+}
